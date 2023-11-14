8f516fe9ee62c247c244d3f0a0c4353e1ecb4b6d
chandrajha <jamresh91@gmail.com>
2023-11-14 07:49:14 +0530

    video-galletry added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VideoGalleryComponent } from './video-gallery/video-gallery.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path:'',component: HomeComponent},
+  {path:'home',component: HomeComponent},
   {path:'video-gallery',component: VideoGalleryComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
 import { FormsModule } from '@angular/forms';
-import { VideoGalleryComponent } from './video-gallery/video-gallery.component'
+import { VideoGalleryComponent } from './video-gallery/video-gallery.component';
+import { HomeComponent } from './home/home.component'
 @NgModule({
   declarations: [
     AppComponent,
-    VideoGalleryComponent
+    VideoGalleryComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
