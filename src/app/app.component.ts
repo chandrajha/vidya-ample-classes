ba5f5b9ff3342526287774261f41a3fef6c8f1a9
chandrajha <jamresh91@gmail.com>
2023-11-16 10:21:53 +0530

    menu toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 interface Food {
   value: string;
@@ -10,7 +10,11 @@ interface Food {
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+    this.addEvent();
+  }
   
   title = 'vidya-ample';
   selectedValue: string='';
@@ -29,5 +33,13 @@ export class AppComponent {
       $('.super-menu').css('display','block');
 
     }
+  }
+
+  addEvent(){
+    $( ".super-menu nav ul li" ).on( "click", function() {
+      if(this.getAttribute('class') !== 'dropdown'){
+        $('#myLinks').css('display','none');  
+      }
+    });
   }
 }
