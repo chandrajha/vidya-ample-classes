30d3c89be8ca42e79ccb1027d5eb78fe75340cb9
chandrajha <jamresh91@gmail.com>
2023-12-07 10:42:53 +0530

    olympid banner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,4 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent { }
+export class HomeComponent { 
+  bannerItems=[
+    {"banner":"../assets/olympiad-banner/banner-2.jpg"},
+    {"banner":"../assets/olympiad-banner/banner-3.jpg"},
+    {"banner":"../assets/olympiad-banner/banner-4.jpg"},
+    {"banner":"../assets/olympiad-banner/banner-5.jpg"},
+    {"banner":"../assets/olympiad-banner/banner-6.jpg"},
+    {"banner":"../assets/olympiad-banner/banner-7.jpg"},
+    {"banner":"../assets/olympiad-banner/banner-8.jpg"},
+    {"banner":"../assets/olympiad-banner/banner-9.jpg"},
+    {"banner":"../assets/olympiad-banner/banner-10.jpg"},
+    {"banner":"../assets/olympiad-banner/banner-11.jpg"},
+    {"banner":"../assets/olympiad-banner/banner-12.jpg"},
+    {"banner":"../assets/olympiad-banner/banner-13.jpg"},
+    {"banner":"../assets/olympiad-banner/banner-14.jpg"},
+    {"banner":"../assets/olympiad-banner/banner-15.jpg"}
+  ]
+ }
